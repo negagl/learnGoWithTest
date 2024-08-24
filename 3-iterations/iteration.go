1b2e2d729408f1d9778c08b6fb28fda514640bfd
Nega García <nega_gl@outlook.com>
2024-08-24 13:09:24 -0500

    Iterations | refactor

--- 3-iterations/iteration.go
+++ 3-iterations/iteration.go
@@ -1,11 +1,9 @@
 package iterations
 
-func Repeat(character string, times int) string {
-	var repeatedString string
-
+func Repeat(character string, times int) (repeatedString string) {
 	for i := 0; i < times; i++ {
-		repeatedString = repeatedString + character
+		repeatedString += character
 	}
 
-	return repeatedString
+	return
 }
