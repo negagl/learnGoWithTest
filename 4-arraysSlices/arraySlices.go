fbaa5630e6577228c268157016fffd94d63dad48
Nega García <nega_gl@outlook.com>
2024-08-24 18:54:41 -0500

    ArraySlices | Accespt any size to sum

--- 4-arraysSlices/arraySlices.go
+++ 4-arraysSlices/arraySlices.go
@@ -1,8 +1,9 @@
 package arrayslices
 
-func Sum(numbers [5]int) (sum int) {
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+func Sum(numbers []int) (sum int) {
+	for _, number := range numbers {
+		sum += number
 	}
+
 	return
 }
--- 4-arraysSlices/arraySlices_test.go
+++ 4-arraysSlices/arraySlices_test.go
@@ -3,12 +3,14 @@ package arrayslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("array of 5 items", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	expected := 15
+		got := Sum(numbers)
+		expected := 15
 
-	if got != expected {
-		t.Errorf("expected %d but got %d, %v", expected, got, numbers)
-	}
+		if got != expected {
+			t.Errorf("expected %d but got %d, %v", expected, got, numbers)
+		}
+	})
 }
