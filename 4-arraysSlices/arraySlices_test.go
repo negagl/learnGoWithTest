529f036aca67d070e71612aad7e7560069537995
Nega García <nega_gl@outlook.com>
2024-08-25 02:09:29 -0500

    ArraySlices | Refactor for TestSumAllTails

--- 4-arraysSlices/arraySlices_test.go
+++ 4-arraysSlices/arraySlices_test.go
@@ -28,20 +28,22 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("want %v but got %v", want, got)
+		}
+	}
 	t.Run("sum all tails", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9, 3, 5})
 		want := []int{2, 17}
 	
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("want %v but got %v", want, got)
-		}
+		checkSums(t, got, want)
 	})
 	t.Run("sum all tails with empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
 	
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("want %v but got %v", want, got)
-		}
+		checkSums(t, got, want)
 	})
 }
