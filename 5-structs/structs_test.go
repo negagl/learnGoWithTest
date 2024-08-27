0cbae72ead7ab5fac824bbbdbe44057cd7557e83
Nega García <nega_gl@outlook.com>
2024-08-27 01:24:01 -0500

    structs | added an interface Shape + refactor

--- 5-structs/structs_test.go
+++ 5-structs/structs_test.go
@@ -15,21 +15,19 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
-
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
-		}
+		CheckArea(t, rectangle, 72.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
-		}
+		CheckArea(t, circle, 314.1592653589793)
 	})
+}
+
+func CheckArea(t testing.TB, shape Shape, want float64) {
+	t.Helper()
+	got := shape.Area()
+	if got != want {
+		t.Errorf("got %g, want %g", got, want)
+	}
 }
