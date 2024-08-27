b43adb483b16b89386838629790352aba0dfbfbc
Nega García <nega_gl@outlook.com>
2024-08-27 01:46:15 -0500

    structs | refactor to TestArea

--- 5-structs/structs_test.go
+++ 5-structs/structs_test.go
@@ -16,18 +16,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want float64
-	} {
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g, want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				// %#v format displays the value in Go syntax, including its type and field names
+				t.Errorf("%#v got %g, want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
