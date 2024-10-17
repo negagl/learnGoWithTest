10a08c2072ed95f25db5ba1731cf2cbf8b9b8a35
Nega García <nega_gl@outlook.com>
2024-10-17 03:21:45 -0500

    pointers | refactor

--- 6-pointers/wallet_test.go
+++ 6-pointers/wallet_test.go
@@ -7,27 +7,31 @@ import (
 // make a Wallet struct which lets us deposit Bitcoin
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(tb testing.TB, wallet Wallet, want Bitcoin) {
+		tb.Helper()
+
+		got := wallet.Balance()
+
+		if got != want {
+			tb.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
 		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(100)}
 		wallet.Withdraw(Bitcoin(30))
 
-		got := wallet.Balance()
 		want := Bitcoin(70)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 }
