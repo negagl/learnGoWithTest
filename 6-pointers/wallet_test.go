7d05b13ebdf258aabceefbdde387950de5f3abbb
Nega García <nega_gl@outlook.com>
2024-10-17 03:16:20 -0500

    pointers | added withdraw function

--- 6-pointers/wallet_test.go
+++ 6-pointers/wallet_test.go
@@ -6,13 +6,28 @@ import (
 
 // make a Wallet struct which lets us deposit Bitcoin
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(1))
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(100)}
+		wallet.Withdraw(Bitcoin(30))
+
+		got := wallet.Balance()
+		want := Bitcoin(70)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
